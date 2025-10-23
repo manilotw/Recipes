2b9b2cf5b649ea0e8961b8f2712cc86f8688f0fe
Amirzhan Albariyev <albariev123@gmail.com>
2025-10-23 11:02:44 +0800

    templates connected w urls

--- favorites/views.py
+++ favorites/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views that render templates in the project's templates/ directory.
+def index(request):
+	return render(request, 'index.html')
+
+
+def auth_view(request):
+	return render(request, 'auth.html')
+
+
+def card1(request):
+	return render(request, 'card1.html')
+
+
+def card2(request):
+	return render(request, 'card2.html')
+
+
+def card3(request):
+	return render(request, 'card3.html')
+
+
+def lk(request):
+	return render(request, 'lk.html')
+
+
+def order(request):
+	return render(request, 'order.html')
+
+
+def registration(request):
+	return render(request, 'registration.html')
