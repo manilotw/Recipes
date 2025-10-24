ba3bc6936bf62296ab09be34a2253ca83a064d19
Amirzhan Albariyev <albariev123@gmail.com>
2025-10-24 23:36:50 +0800

    Added MealTariff model

--- recipe/settings.py
+++ recipe/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from environs import env
+from environs import Env
 import os
 
+from environs import Env
 
+env = Env()
 env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,12 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', True)
-
+DEBUG = env.bool('DEBUG', default=True)
 ALLOWED_HOSTS = []
 
 
